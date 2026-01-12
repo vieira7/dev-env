46777e276bc2f899e85aaf8c42f6e6826e77582e
João Vieira <38434855+vieira7@users.noreply.github.com>
2026-01-12 13:04:53 +0000

    feat: check for and install borders after aerospace

--- macOS/aerospace/up.sh
+++ macOS/aerospace/up.sh
@@ -6,7 +6,13 @@ REPO_DIR=$(git rev-parse --show-toplevel)
 
 # install aerospace
 if ! brew list --cask aerospace &>/dev/null; then
-    brew install --cask aerospace
+    brew install --cask nikitabobko/tap/aerospace
+fi
+
+# install borders
+if ! brew list borders &>/dev/null; then
+    brew tap FelixKratz/formulae
+    brew install borders
 fi
 
 # copy aerospace config
